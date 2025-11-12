22a904ccb39657d0b65ae896cf98d998ff849c25
nestdragonx <nestdragonx@gmail.com>
2025-11-11 23:27:56 -0800

    'perbaikan button

--- assets/Javascript/login.js
+++ assets/Javascript/login.js
@@ -1,31 +1,30 @@
 import { BACKEND_URL } from "./config.js";
 
-loginBtn.addEventListener("click", async () => {
-  if (!adminPass.value || adminPass.value.trim() === "" || !adminUsername.value || adminUsername.value.trim() === "") return (loginError.textContent = "⚠️ Username dan Password wajib diisi.");
+document.getElementById("loginBtn").addEventListener("click", async () => {
+  const password = document.getElementById("adminPass").value;
+
   try {
     const res = await fetch(`${BACKEND_URL}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username: adminUsername.value,password: adminPass.value }),
-      credentials: 'include'
+      body: JSON.stringify({ password })
     });
 
     const data = await res.json();
-    if (res.ok && data.success) {
+
+    if (res.ok) {
+      localStorage.setItem("authToken", data.token);
       window.location.href = "dashboard.html";
-    } else {
-      loginError.textContent = "❌ Username atau Password salah,\nSilahkan coba lagi.";
     }
   } catch (err) {
     console.error(err);
-    loginError.textContent = "⚠️ Gagal terhubung ke server. Coba lagi nanti.";
+    alert("Gagal terhubung ke server backend.");
   }
 });
 
-
 // window di load check token valid
-(async () => {
-  try {
+    (async () => {
+      try {
         const res = await fetch(`${BACKEND_URL}/verifyToken`, {
           method: "GET",
           credentials: 'include'
@@ -44,6 +43,27 @@ loginBtn.addEventListener("click", async () => {
     const adminPass = document.getElementById("adminPass");
     const loginError = document.getElementById("loginError");
 
+    loginBtn.addEventListener("click", async () => {
+      if (!adminPass.value || adminPass.value.trim() === "" || !adminUsername.value || adminUsername.value.trim() === "") return (loginError.textContent = "⚠️ Username dan Password wajib diisi.");
+      try {
+        const res = await fetch(`${BACKEND_URL}/login`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username: adminUsername.value,password: adminPass.value }),
+          credentials: 'include'
+        });
+
+        const data = await res.json();
+        if (res.ok && data.success) {
+          window.location.href = "dashboard.html";
+        } else {
+          loginError.textContent = "❌ Username atau Password salah,\nSilahkan coba lagi.";
+        }
+      } catch (err) {
+        console.error(err);
+        loginError.textContent = "⚠️ Gagal terhubung ke server. Coba lagi nanti.";
+      }
+    });
 
     // Tekan Enter untuk login
     adminPass.addEventListener("keypress", (e) => {
